00d7fc34b0d835beefea8a88a4f060f1af0f263e
see-bet-code <brittani.elan.taylor@gmail.com>
2020-09-30 13:57:55 -0400

    formatting fixes to yelp api

--- lib/yelp_api_adapter.rb
+++ lib/yelp_api_adapter.rb
@@ -82,10 +82,10 @@ class YelpAPIAdapter
 
     options = {}
     OptionParser.new do |opts|
-    opts.banner = "Example usage: ruby sample.rb (search|lookup) [options]"
+        opts.banner = "Example usage: ruby sample.rb (search|lookup) [options]"
 
-    opts.on("-tTERM", "--term=TERM", "Search term (for search)") do |term|
-        options[:term] = term
+        opts.on("-tTERM", "--term=TERM", "Search term (for search)") do |term|
+            options[:term] = term
     end
 
     opts.on("-lLOCATION", "--location=LOCATION", "Search location (for search)") do |location|
@@ -100,35 +100,35 @@ class YelpAPIAdapter
         puts opts
         exit
     end
-    end.parse!
+end.parse!
 
 
-    command = ARGV
+command = ARGV
 
 
-    case command.first
+case command.first
     when "search"
-    term = options.fetch(:term, DEFAULT_TERM)
-    location = options.fetch(:location, DEFAULT_LOCATION)
+        term = options.fetch(:term, DEFAULT_TERM)
+        location = options.fetch(:location, DEFAULT_LOCATION)
 
-    raise "business_id is not a valid parameter for searching" if options.key?(:business_id)
+        raise "business_id is not a valid parameter for searching" if options.key?(:business_id)
 
-    response = search(term, location)
+        response = search(term, location)
 
-    puts "Found #{response["total"]} businesses. Listing #{SEARCH_LIMIT}:"
-    response["businesses"].each {|biz| puts biz["name"]}
+        puts "Found #{response["total"]} businesses. Listing #{SEARCH_LIMIT}:"
+        response["businesses"].each {|biz| puts biz["name"]}
     when "lookup"
-    business_id = options.fetch(:business_id, DEFAULT_BUSINESS_ID)
+        business_id = options.fetch(:business_id, DEFAULT_BUSINESS_ID)
 
 
-    raise "term is not a valid parameter for lookup" if options.key?(:term)
-    raise "location is not a valid parameter for lookup" if options.key?(:lookup)
+        raise "term is not a valid parameter for lookup" if options.key?(:term)
+        raise "location is not a valid parameter for lookup" if options.key?(:lookup)
 
-    response = business(business_id)
+        response = business(business_id)
 
-    puts "Found business with id #{business_id}:"
-    puts JSON.pretty_generate(response)
+        puts "Found business with id #{business_id}:"
+        puts JSON.pretty_generate(response)
     else
-    puts "Please specify a command: search or lookup"
+        puts "Please specify a command: search or lookup"
     end
 end
